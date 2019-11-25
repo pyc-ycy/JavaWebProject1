a2c082066af7106dcd5e7c2582e08c8cacdb90de
御承扬 <2923616405@qq.com>
2019-11-25 19:34:10 +0800

    Update

---
+++ src/com/lyq/ServletFilterSet/AddServlet.java
@@ -0,0 +1,35 @@
+package com.lyq.ServletFilterSet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "AddServlet")
+public class AddServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        String id = request.getParameter("id");
+        String name = request.getParameter("name");
+        String author = request.getParameter("author");
+        String price = request.getParameter("price");
+        out.print("<h2>图书信息添加成功</h2><hr>");
+        out.println("<center>");
+        out.println("<p>");
+        out.print("图书编号：" + id + "<br>");
+        out.print("图书名称：" + name + "<br>");
+        out.print("图书作者：" + author + "<br>");
+        out.print("图书价格：" + price + "<br>");
+        out.println("</p>");
+        out.println("</center>");
+        out.flush();
+        out.close();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+}
