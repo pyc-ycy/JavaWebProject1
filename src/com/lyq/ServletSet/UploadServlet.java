5861ad2f21f2085dc7de4b4aa3acbb56a3873226
ycy-ycy <2923616405@qq.com>
2019-11-30 09:39:51 +0800

    Update

--- src/com/lyq/ServletSet/UploadServlet.java
+++ src/com/lyq/ServletSet/UploadServlet.java
@@ -1,10 +1,12 @@
-package com.lyq.ServletSet;//@Software: IntelliJ IDEA
+
+//@Software: IntelliJ IDEA
 // @Project: JavaWebProject1
 //@File:${NAME}
 //@Date:2019/11/29
 // Author:御承扬
 //E-mail:2923616405@qq.com
 
+package com.lyq.ServletSet;
 import org.apache.catalina.core.ApplicationPart;
 
 import javax.servlet.ServletException;
@@ -17,8 +19,8 @@ import javax.servlet.http.Part;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "UploadServlet", urlPatterns = "/UploadServlet")
 @MultipartConfig(location = "D:\\users")
+@WebServlet(name = "UploadServlet", urlPatterns = "/UploadServlet")
 public class UploadServlet extends HttpServlet {
     private static final long serialVersionUID=1L;
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -29,13 +31,8 @@ public class UploadServlet extends HttpServlet {
         Part p = request.getPart("file1");
         if(p.getContentType().contains("image")){
             ApplicationPart ap = (ApplicationPart) p;
-            //String fname1 = ap.getSubmittedFileName();
-            //int path_idx = fname1.lastIndexOf("\\");
-            //String fname2 = fname1.substring(path_idx);
-            //String fname2 = "IMG_20180228_155857_1.jpg";
             String fname2 = request.getParameter("rename");
             p.write(path + "/image/"+fname2+".jpg");
-//            p.write(path+"/image");
             out.write("文件上传成功");
         }else{
             out.write("请选择图片！！！！");
